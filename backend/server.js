998c9b05f460bb55c1d69c5cbfb8d3859234ab34
pi8878 <simangolwalifwatila99@gmail.com>
2024-10-10 18:25:36 -0500

    changes to database

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB (replace with your MongoDB URI)
-const uri = 'mongodb://localhost:27017/churchdb';
+// const uri = 'mongodb://localhost:27017/churchdb';
+const uri = 'mongodb+srv://simangolwalifwatila99:<2e0QrcJveY61SqkA>@cluster0.fqsyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
